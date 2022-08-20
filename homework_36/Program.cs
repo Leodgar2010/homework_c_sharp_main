570c2e9f00a965587ba6389fe06eaa6cf8f235ae
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-20 16:50:17 +0300

    Внес исправления по замечаниям ревьювера

--- homework_36/Program.cs
+++ homework_36/Program.cs
@@ -3,32 +3,23 @@
 //  элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
-int[] createArray()
+int[] CreateArray(int size, int min , int max)
 {
-    Console.WriteLine ("Введите размер массива: ");
-    var numberString = Console.ReadLine();
-    int number = int.Parse(numberString!);
-    Console.WriteLine ("Введите минимальное значение массива: ");
-    var number2String = Console.ReadLine();
-    int number2 = int.Parse(number2String!);
-    Console.WriteLine ("Введите максимальное значение массива: ");
-    var number3String = Console.ReadLine();
-    int number3 = int.Parse(number3String!);
     var random = new Random();
-    var result = new int[number];
+    var result = new int[size];
     for (int i = 0; i < result.Length; i++)
     {
-        result[i] = random.Next(number2,number3);
+        result[i] = random.Next(min,max);
     }
     return result;
 }
-string print_array_numbers (int [] arr)
+string PrintArrayNumbers (int [] arr)
 {
     string arrStr = string.Join(", ", arr);
     string result = string.Concat("[", arrStr, "]");
     return result;
 }
-int sum_odd_array (int [] arr)
+int SumOddArray (int [] arr)
 {
     int result =0;
     for (int i=1; i<arr.Length; i+=2)
@@ -38,5 +29,14 @@ int sum_odd_array (int [] arr)
     return result;
 }
 Console.WriteLine ("Какова сумма нечётных элементов данного массива?");
-int [] array = createArray (); 
-Console.WriteLine ("Сумма нечетных элементов массива "+ print_array_numbers(array) + " равна "+ sum_odd_array(array)+".");
+ Console.WriteLine ("Введите размер массива: ");
+    var numberString = Console.ReadLine();
+    int number = int.Parse(numberString!);
+    Console.WriteLine ("Введите минимальное значение массива: ");
+    var number2String = Console.ReadLine();
+    int number2 = int.Parse(number2String!);
+    Console.WriteLine ("Введите максимальное значение массива: ");
+    var number3String = Console.ReadLine();
+    int number3 = int.Parse(number3String!);
+int [] array = CreateArray (number,number2,number3); 
+Console.WriteLine ("Сумма нечетных элементов массива "+ PrintArrayNumbers(array) + " равна "+ SumOddArray(array)+".");
