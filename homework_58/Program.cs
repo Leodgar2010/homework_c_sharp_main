b3971a2808d4c99a7442554641aff3027b29a780
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-31 10:39:04 +0300

    Начал решать домашнюю работу к восьмому семинару

---
+++ homework_58/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+// (6+12) (12+8)
+// (9+6) (12+6)
+int[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    int[,] result = new int[m, n];
+    var random = new Random();
+    for (int i = 0; i < result.GetLength(0); i++)
+        for (int j = 0; j < result.GetLength(1); j++)
+            result[i, j] = random.Next(1, 10);
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++) Console.Write($"{array[i, j]}, ");
+        Console.WriteLine();
+    }
+}
+int[,] MultiplicationArray(int[,] firstArray, int[,] secondArray)
+{
+    int sum=0;
+    int resultColumn = 0;
+    int[,] result = new int[firstArray.GetLength(0), firstArray.GetLength(1)];
+    for (int i=0; i<firstArray.GetLength (0); i++)
+        {
+            for (int j=0; j<firstArray.GetLength (0); j++) 
+            {
+                sum += firstArray [i,j]*secondArray [j,i]; 
+            }
+            Console.WriteLine (sum);          
+            result [i,resultColumn]=sum;
+            resultColumn ++;
+            sum = 0;       
+        }
+    
+    return result;
+}
+Console.WriteLine("Произведение первого массива");
+int[,] firstArray = CreateArrayWithRandomNumbers(3, 3);
+PrintArray(firstArray);
+Console.WriteLine("на второй массив");
+int[,] secondArray = CreateArrayWithRandomNumbers(3, 3);
+PrintArray(secondArray);
+Console.WriteLine("равняется");
+PrintArray(MultiplicationArray(firstArray, secondArray));
